fe48944dda509966d762301733ce602523564d97
Boocie-N <contact.kesed@gmail.com>
2023-07-21 17:32:50 +0200

    added html and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
-import './App.css';
-import Weather from './Weather';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
@@ -8,18 +7,33 @@ function App() {
       <div className="container">
         <h1>Hello world</h1>
         <Weather />
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://snazzy-alfajores-066529.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Boocie{" "}
-        </a>{" "}
-        and is open-sourced on <a href=''GitHub and hosted on Netlify
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://snazzy-alfajores-066529.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Boocie{" "}
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Boocie-N/my-weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://magnificent-moxie-bf1bae.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -4,20 +4,42 @@ import "./Weather.css";
 function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
       <div className="row">
         <div className="col-6">
           <img
             src="https://vanilla-weather-app-andi.netlify.app/img/04d.png"
             alt="moslty cloudy"
+            className="current-weather-icon"
           />
-          Pretoria
+          <span className="city-name">Pretoria</span>
+
           <ul>
             <li>15:34, Sat Jul 21</li>
             <li>Overcast clouds</li>
           </ul>
         </div>
         <div className="col-6">
-          <h3>12°C</h3>
+          <h3>
+            12<span className="units">°C | °F</span>
+          </h3>
           <ul>
             <li>Humidity: 70%</li>
             <li>Wind: 5km/h</li>
